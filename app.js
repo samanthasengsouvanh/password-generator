0e8fa74cf4e48356b662bc14e489bdd86c83dc25
Samantha Sengsouvanh <samantha.sengsouvanh@gmail.com>
2019-11-17 18:51:38 -0600

    updated html with checkboxes

--- app.js
+++ app.js
@@ -1,102 +1,31 @@
-const specialChar = [
-    "@",
-    "%",
-    "+",
-    "\\",
-    "/",
-    "'",
-    "!",
-    "#",
-    "$",
-    "^",
-    "?",
-    ":",
-    ",",
-    ")",
-    "(",
-    "}",
-    "{",
-    "]",
-    "[",
-    "~",
-    "-",
-    "_",
-    "."
+const numericChar = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"
 ];
-const numericChar = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
-const lowerCaseChar = [
-    "a",
-    "b",
-    "c",
-    "d",
-    "e",
-    "f",
-    "g",
-    "h",
-    "i",
-    "j",
-    "k",
-    "l",
-    "m",
-    "n",
-    "o",
-    "p",
-    "q",
-    "r",
-    "s",
-    "t",
-    "u",
-    "v",
-    "w",
-    "x",
-    "y",
-    "z"
+
+const specialChar = ["!", "@", "#", "$", "%", "^", "&", "*"
 ];
-const upperCaseChar = [
-    "A",
-    "B",
-    "C",
-    "D",
-    "E",
-    "F",
-    "G",
-    "H",
-    "I",
-    "J",
-    "K",
-    "L",
-    "M",
-    "N",
-    "O",
-    "P",
-    "Q",
-    "R",
-    "S",
-    "T",
-    "U",
-    "V",
-    "W",
-    "X",
-    "Y",
-    "Z"
+
+const lwChar = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"
 ];
+const upChar = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"
+];
+
 let arrayToUse = [];
 
 
 //This function is to prompt user for password options.
 function getPasswordChoices() {
 
-    const length = parseInt(prompt("How many characters would you like in your password?"));
+    const length = parseInt(prompt("How many characters would you like in your password?")
+    );
     //Minimum character length for password
     if (length < 8) {
         alert("Password should be at least 8 characters");
-        return;
     }
     //Maximum character length for password
     if (length > 128) {
         alert("Password length must be less than 129 characters");
     }
-    //if Not a Number methis is true, user must provide correct length for password
+    //if Not a Number is true, user must provide correct length for password
     if (isNaN(length) == true) {
         alert("You should provide correct length for password");
     }
@@ -104,35 +33,42 @@ function getPasswordChoices() {
     const hasSpecialChar = confirm("Click ok to confirm incuding special characters");
     console.log("hasSpecialCharacter: " + hasSpecialChar);
     const hasNumericChar = confirm(" Click ok to confirm incuding numeric characters");
-    const hasLowerCaseChar = confirm(" Click ok to confirm incuding lower case characters");
-    const hasUpperCaseChar = confirm(" Click ok to confirm incuding upper case characters");
+    console.log("hasNumericChar: " + hasNumericChar);
+    const hasLwChar = confirm(" Click ok to confirm incuding lower case characters");
+    console.log("hasLwChar: " + hasLwChar);
+    const hasUpChar = confirm(" Click ok to confirm incuding upper case characters");
+    console.log("hasUpChar: " + hasUpChar);
 
     //if each value is false, alert must prompt user to select at least one character type
-    if (hasSpecialChar === false && hasNumericChar === false && hasLowerCaseChar === false && hasUpperCaseChar === false) {
+    if (hasSpecialChar === false && hasNumericChar === false && hasLwChar === false && hasUpChar === false) {
         alert("You must select at least one character type");
     }
     if (hasSpecialChar){
         arrayToUse = arrayToUse.concat(specialChar);
         console.log(specialChar);
-        console.log(arrayToUse)
+        console.log(arrayToUse);
     }
     if (hasNumericChar){
         arrayToUse = arrayToUse.concat(numericChar);
-        console.log(arrayToUse)
+        console.log(numericChar);
+        console.log(arrayToUse);
     }
-    if (hasLowerCaseChar){
-        arrayToUse = arrayToUse.concat(lowerCaseChar)
-        console.log(arrayToUse)
+    if (hasLwChar){
+        arrayToUse = arrayToUse.concat(lwChar)
+        console.log(lwChar);
+        console.log(arrayToUse);
     }
-    if (hasUpperCaseChar){
-        arrayToUse = arrayToUse.concat(upperCaseChar)
-        console.log(arrayToUse)
+    if (hasUpChar){
+        arrayToUse = arrayToUse.concat(upChar)
+        console.log(upChar);
+        console.log(arrayToUse);
     }
+
     //value of each userOption
     const userOptions = {
         length: length,
-        hasLowerCaseChar: hasLowerCaseChar,
-        hasUpperCaseChar: hasUpperCaseChar,
+        hasLwChar: hasLwChar,
+        hasUpChar: hasUpChar,
         hasNumericChar: hasNumericChar,
         hasSpecialChar: hasSpecialChar,
     }
@@ -154,9 +90,8 @@ function getPasswordChoices() {
         let guaranteedChar = [];
 
         if (options.hasSpecialChar) {
-            possibleChar = getRandom(specialChar);
-            guaranteedChar.push(getRandom(specialChar));
-
+            possibleChar = possibleChar.concat(specialChar);
+            guaranteedChar.push(concat(specialChar));
         }
         for (let i = 0; i < options.length; i++) {
             possibleChar = getRandom(possibleChar);
