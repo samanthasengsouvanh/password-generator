45ff6e96e51ca63fa51db4d0bf16ef113cc5d938
Samantha Sengsouvanh <samantha.sengsouvanh@gmail.com>
2019-12-09 20:42:34 -0600

    working app uploaded

--- app2.js
+++ app2.js
@@ -1,20 +1,70 @@
-const lwChar = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"
-];
-const upChar = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"
-];
+const lwChar = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 
-const numericChar = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"
-];
+const upChar = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 
-const specChar = ["!", "@", "#", "$", "%", "^", "&", "*"
-];
+const numericChar = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+
+const specChar = ["!", "@", "#", "$", "%", "^", "&", "*"];
 
 let arrayToUse = [];
+let password = document.getElementById("Password");
 
-function getPasswordChoices(){
-    const length = parseInt(prompt("How many characters would you like in your password?"))
+function getPasswordChoices() {
+    let p=0;
+    let finalPassword = [];
+
+    console.log("button");
+
+    //const lwChar = document.getElementById(lwChar);
+    //console.log(lwChar)
+    if (document.getElementById("lwChar").checked){ {
+        arrayToUse = arrayToUse.concat(lwChar);
+        console.log(arrayToUse);
+
+        let lowerChar = lwChar[Math.floor(Math.random() * lwChar.length)];
+
+        finalPassword.push(lowerChar);
+
+        p+=1
+    }
+    //const upChar = document.getElementById(upChar);
+    //console.log(upChar)
+    if (document.getElementById("upChar").checked) {
+        arrayToUse = arrayToUse.concat(upChar)
+        console.log(arrayToUse)
+
+        let upperChar = upChar[Math.floor(Math.random() * upChar.length)];
+
+        finalPassword.push(upperChar);
+
+        p+=1
+    }
+    //const numericChar = document.getElementById(numericChar);
+    //console.log(numericChar)
+    if (document.getElementById("numericChar").checked) {
+        arrayToUse = arrayToUse.concat(numericChar)
+
+        let numChar = numericChar[Math.floor(Math.random() * numericChar.length)];
+
+        finalPassword.push(numChar);
+
+        p+=1
+    }
+    //const specChar = document.getElementById(specChar);
+    //console.log(specChar)
+    if (document.getElementById("specChar").checked) {
+        arrayToUse = arrayToUse.concat(specChar)
+
+        let specialChar = specChar[Math.floor(Math.random() * specChar.length)];
+
+        finalPassword.push(specialChar);
+
+        p+=1
+    }
+    const length = document.getElementById("length").value
     //Minimum character length for password
     if (length < 8) {
+        alert("Password length must be greater than 8 characters");
     }
     //Maximum character length for password
     if (length > 128) {
@@ -24,26 +74,28 @@ function getPasswordChoices(){
     if (isNaN(length) == true) {
         alert("You should provide correct length for password");
     }
-    const lwChar = document.getElementById(lwChar);
-    console.log(lwChar)
-    if (lwChar){
-        arrayToUse = arrayToUse.concat(lwChar)
-        console.log(arrayToUse)
+
+    for (let i = 0; i + p < length; i++) {
+
+        //select random char from password array
+
+        let passwordChar = arrayToUse[Math.floor(Math.random() * arrayToUse.length)];
+
+        console.log(passwordChar);
+
+        //push random char into final password
+
+        finalPassword.push(passwordChar);
     }
-    const upChar = document.getElementById(upChar);
-    console.log(upChar)
-    if (upChar){
-        arrayToUse = arrayToUse.concat(upChar)
-        console.log(arrayToUse)
-    }
-    const numericChar = document.getElementById(numericChar);
-    console.log(numericChar)
-    if (numericChar){
-        arrayToUse = arrayToUse.concat(numericChar)
-    }
-    const specChar = document.getElementById(specChar);
-    console.log(specChar)
-    if (specChar){
-        arrayToUse = arrayToUse.concat(specChar)
-    }
+    console.log(finalPassword);
+    password.innerHTML = finalPassword.join("");
+
+
+}
+}
+function copyPassword() {
+const copiedPassword = document.getElementById("Password");
+copiedPassword.select();
+copiedPassword.setSelectionRange(0, 99999)
+document.execCommand("copy");
 }
